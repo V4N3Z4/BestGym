ffae16337410c944e8d09e0830f047294e6c849f
V4N3Z4 <vanessa.zamorano@yh.nackademin.se>
2025-10-24 08:12:34 +0200

    Member.java

--- src/Member.java
+++ src/Member.java
@@ -1,15 +1,21 @@
 import java.time.LocalDate;
 
 public class Member {
-    private String name;
-    private String personalNumber;
-    private LocalDate lastPaymentDate;
-    private String membershipLevel;
+    private final String name;
+    private final String address;
+    private final String email;
+    private final String personalNumber;
+    private final LocalDate membershipStartDate;
+    private final LocalDate lastPaymentDate;
+    private final String membershipLevel;
 
     public Member(String name, String address, String email, String personalNumber,
                   LocalDate membershipStartDate, LocalDate lastPaymentDate, String membershipLevel) {
         this.name = name;
+        this.address = address;
+        this.email = email;
         this.personalNumber = personalNumber;
+        this.membershipStartDate = membershipStartDate;
         this.lastPaymentDate = lastPaymentDate;
         this.membershipLevel = membershipLevel;
     }
